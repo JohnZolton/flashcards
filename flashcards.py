8c63fed321810a07a39222efc8c4d305208d74d2
John Zolton <102374100+JohnZolton@users.noreply.github.com>
2022-09-15 13:27:52 -0400

    Add files via upload
    high level overview

---
+++ flashcards.py
@@ -0,0 +1,19 @@
+"""
+This is a flashcard app, inspired by anki and Dr. Andrew Huberman
+
+GUI: 
+    Stage 1: show answer
+    Stage 2: rate answer easy/medium/hard
+        click/keystroke -> next card
+cards:
+    front
+    back
+    spaced repetition marker 
+
+random scary sound + picture
+    shown at random after you click "next card"
+
+* release of adrenaline tells your neurons to remember the events directly
+before the release of adrenaline, flashing a scary image + sound may
+startle you and release some adrenaline, helping you remember
+"""
