102b9fd91f0e27a83df4e958a7da2a1a80f10293
John Zolton <102374100+JohnZolton@users.noreply.github.com>
2022-09-18 11:04:58 -0400

    Add files via upload
    added progress bar

--- flashcards.py
+++ flashcards.py
@@ -1,51 +1,30 @@
-"""
-This is a flashcard app, inspired by anki and Dr. Andrew Huberman
-
-GUI: 
-    Stage 1: show answer
-    Stage 2: rate answer easy/medium/hard
-        click/keystroke -> next card
-    Stage 3: edit card - front/back
-             delete card
-
-cards:
-    front
-    back
-    spaced repetition marker 
-    * would be cool if you could import anki decks
-
-random scary sound + picture
-    shown at random after you click "next card"
-
-90-minute timer? display/hide
-
-semi-random 10 second breaks
-
-* Release of adrenaline tells your neurons to remember the events directly
-before the release of adrenaline, flashing a scary image + sound may
-startle you and release some adrenaline, helping you remember what you
-are studying. Maybe later I'll wire this up to a shock collar.
-"""
-
 from tkinter import *
 from tkinter.ttk import *
 import random
+import time
 
-# exported anki deck is {front} {back} {number}
+# exported anki deck is "{front}     {back}       {number}"
 # read cards from text file into list of cards
 flashcards = []
 with open('Spanish Vocabulary Top 5000 2.txt') as f:
     for row in f:
         card = row.split("		") # anki decks are tab delineated
-        if len(card) != 3: continue
+        if len(card) != 3: 
+            continue # skip non-compliant cards
         flashcards.append(card)
 flashcards.sort(key= lambda x:int(x[2]))
 
-# select 10 cards
-cards = random.sample(flashcards, 10)
+# select X cards to study
+curr_deck = 10
+cards = random.sample(flashcards, curr_deck)
 
-
+# TODO have user select deck from GUI
+# TODO progress bar
+# TODO set deck size
+# TODO edit card
 # TODO save progress / studied cards
+# TODO random 10 second pauses
+# TODO play a scary noise + picture randomly
 
 flashcard = random.choice(cards)
 
@@ -65,35 +44,58 @@ def hidebuttons():
 def logeasy():
     # update card rating // time to see again
     # move to next card
-    cards.remove(flashcard)
+    global flashcard
+    cards.remove(flashcard) # card was easy so remove from pile
+    flashcard = random.choice(cards)
     lbl_display['text'] = flashcard[0]
     hidebuttons()
+    bar()
+
 
 def logmedium():
     #TODO update card rating // time to see again
     # move to next card
+    global flashcard
+    flashcard = random.choice(cards)
     lbl_display['text'] = flashcard[0]
     hidebuttons()
 
+
+
 def loghard():
-    # update card rating // time to see again
+    #TODO update card rating // time to see again
     # move to next card
+    global flashcard
+    flashcard = random.choice(cards)
     lbl_display['text'] = flashcard[0]
     hidebuttons()
+
 
 window = Tk()
 window.title('Scanki')
 window.columnconfigure(0, minsize=500)
-window.rowconfigure([0,1], minsize=100)
+window.rowconfigure([0,1], minsize=50)
 
-frame1 = Frame(window, padding='3 3 12 12')
-frame2= Frame(borderwidth=1)
+frame1 = Frame(window)
+frame2= Frame(window, borderwidth=1)
 
-frame1.grid(padx= 5, pady= 5, row=0, column= 0)
-frame2.grid(row=1, column=0, sticky='s')
+frame0 = Frame(window, height= 10)
+frame0.grid(row = 0, column = 0, sticky='N')
+frame1.grid(row=1, column= 0)
+
+frame2.grid(row=2, column=0, sticky='s', pady=10)
+
+# implementing a progress bar
+bar_progress = Progressbar(frame0, orient = HORIZONTAL,
+        length = 100, mode = 'determinate')
+def bar():
+    global cards
+    bar_progress['value'] = 100*(curr_deck - len(cards))/curr_deck
+bar_progress.pack(pady=10)
 
 lbl_display = Label(master=frame1, text = flashcard[0])
-lbl_display.pack()
+#lbl_display.config(height=3, width=20)
+lbl_display.pack(expand=YES, fill= BOTH)
 
 showans = Button(master=frame2, text= "Show Answer", command=showanswer)
 showans.pack()
@@ -102,5 +104,7 @@ showans.pack()
 btn_hard = Button(master=frame2, text = "Hard", command=loghard)
 btn_medium = Button(master=frame2, text = "Medium", command=logmedium)
 btn_easy = Button(master=frame2, text ="Easy", command=logeasy)
+
+
 
 window.mainloop()
