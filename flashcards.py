05f4d28ea631151b4bd38f7de8db0b081f8a7e1f
John Zolton <102374100+JohnZolton@users.noreply.github.com>
2022-09-22 08:13:20 -0400

    Add files via upload
    User can now edit front and back of cards and select number of cards to study

--- flashcards.py
+++ flashcards.py
@@ -4,6 +4,7 @@ from tkinter import simpledialog
 from tkinter.ttk import *
 import random
 
+
 def pause():
     if random.randint(1,100) < 15:
         showans.pack_forget()
@@ -29,19 +30,24 @@ def hidebuttons():
     showans.pack()
 
 def logeasy():
-    # update card rating // time to see again
-    # move to next card
+    # remove card and move to next card
     global flashcard
-    cards.remove(flashcard) # card was easy so remove from pile
-    flashcard = random.choice(cards)
-    lbl_display['text'] = flashcard[0]
-    hidebuttons()
-    bar()
-    pause()
+    if len(cards) >= 2:
+        cards.remove(flashcard) # card was easy so remove from pile
+        flashcard = random.choice(cards)
+        lbl_display['text'] = flashcard[0]
+        hidebuttons()
+        bar()
+        pause()
+    else:
+        lbl_display['text'] = "CONGRATULATIONS, STUDY COMPLETED"
+        hidebuttons()
+        showans.pack_forget()
+        bar_progress['value'] = 100
+
 
 
 def logmedium():
-    #TODO update card rating // time to see again
     # move to next card
     global flashcard
     flashcard = random.choice(cards)
@@ -51,13 +57,21 @@ def logmedium():
 
 
 def loghard():
-    #TODO update card rating // time to see again
     # move to next card
     global flashcard
     flashcard = random.choice(cards)
     lbl_display['text'] = flashcard[0]
     hidebuttons()
     pause()
+
+def edit():
+    global flashcard
+    new_front = simpledialog.askstring("Front", "New Front Card")
+    new_back = simpledialog.askstring("Back", "New Back Card")
+    if new_front:
+        flashcard[0] = new_front
+    if new_back:
+        flashcard[1] = new_back
 
 
 # exported anki deck is "{front}     {back}       {number}"
@@ -76,16 +90,16 @@ with open(path) as f:
 curr_deck = int(simpledialog.askfloat('Cards to study', 'Cards to study'))
 cards = random.sample(flashcards, curr_deck)
 
-# TODO edit card
+
 # TODO save progress / studied cards
 # TODO play a scary noise + picture randomly
-# TODO make it dark mode // prettier
 
 flashcard = random.choice(cards)
 
 
 window = Tk()
-window.title('Scanki')
+
+window.title('Anki Clone')
 window.columnconfigure(0, minsize=500)
 window.rowconfigure([0,1], minsize=50)
 
@@ -94,6 +108,7 @@ frame2= Frame(window, borderwidth=1)
 
 frame0 = Frame(window, height= 10)
 frame0.grid(row = 0, column = 0, sticky='N')
+
 frame1.grid(row=1, column= 0)
 
 frame2.grid(row=2, column=0, sticky='s', pady=10)
@@ -104,7 +119,7 @@ bar_progress = Progressbar(frame0, orient = HORIZONTAL,
 def bar():
     global cards
     bar_progress['value'] = 100*(curr_deck - len(cards))/curr_deck
-bar_progress.pack(pady=10)
+bar_progress.grid(row = 0, column=2, sticky=E, padx=100, pady=10)
 
 # main display, alternates between frontcard, backcard, and 'rest'
 lbl_display = Label(master=frame1, text = flashcard[0])
@@ -120,5 +135,8 @@ btn_hard = Button(master=frame2, text = "Hard", command=loghard)
 btn_medium = Button(master=frame2, text = "Medium", command=logmedium)
 btn_easy = Button(master=frame2, text ="Easy", command=logeasy)
 
+# edit button to edit front/back of card
+btn_edit = Button(master= frame0, text= "Edit", command=edit)
+btn_edit.grid(row= 0, column=0, sticky= W, padx=100, pady=10)
 
 window.mainloop()
